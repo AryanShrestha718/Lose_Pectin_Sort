a20ad2bc2ddd7ceacb92ece38f74ca670f7a8030
AryanShrestha718 <125922412+AryanShrestha718@users.noreply.github.com>
2024-08-26 12:43:06 -0400

    Add files via upload

--- New folder (2)/sorting.js
+++ New folder (2)/sorting.js
@@ -1,33 +1,34 @@
-var points;
+var score;
 var gameState = idle;
+var rand;
 function preload(){
 
     // loading in the different files
 
-    pectin = loadImage("assets/Amylopectin.jpg")
-    pectin2 = loadImage("assets/Amylopectin2.png")
-    pectin3 = loadImage("assets/Amylopectin3.png")
-    pectin4 = loadImage("assets/Amylopectin4.jpg")
-    pectin5 = loadImage("assets/Amylopectin5.png")
-    pectin6 = loadImage("assets/Amylopectin6.png")
-    pectin7 = loadImage("assets/Amylopectin7.png")
-    pectin8 = loadImage("assets/Amylopectin8.png")
-    pectin9 = loadImage("assets/Amylopectin9.png")
-    pectin10 = loadImage("assets/Amylopectin10.png")
-    pectin11 = loadImage("assets/Amylopectin11.jpg")
-    pectin12 = loadImage("assets/Amylopectin12.jpg")
-    lose = loadImage("assets/Amylose.jpg")
-    lose2 = loadImage("assets/Amylose2.png")
-    lose3 = loadImage("assets/Amylose3.png")
-    lose4 = loadImage("assets/Amylose4.jpg")
-    lose5 = loadImage("assets/Amylose5.png")
-    lose6 = loadImage("assets/Amylose6.png")
-    lose7 = loadImage("assets/Amylose7.jpg")
-    lose8 = loadImage("assets/Amylose8.png")
-    lose9 = loadImage("assets/Amylose9.png")
-    lose10 = loadImage("assets/Amylose10.png")
-    lose11 = loadImage("assets/Amylose11.png")
-    lose12 = loadImage("assets/Amylose12.png")
+    1 = loadImage("Assets/Amylopectin.jpg")
+    2 = loadImage("Assets/Amylopectin2.png")
+    3 = loadImage("Assets/Amylopectin3.png")
+    4 = loadImage("Assets/Amylopectin4.jpg")
+    5 = loadImage("Assets/Amylopectin5.png")
+    6 = loadImage("Assets/Amylopectin6.png")
+    7 = loadImage("Assets/Amylopectin7.png")
+    8 = loadImage("Assets/Amylopectin8.png")
+    9 = loadImage("Assets/Amylopectin9.png")
+    10 = loadImage("Assets/Amylopectin10.png")
+    11 = loadImage("Assets/Amylopectin11.jpg")
+    12 = loadImage("Assets/Amylopectin12.jpg")
+    13 = loadImage("Assets/Amylose.jpg")
+    14 = loadImage("Assets/Amylose2.png")
+    15 = loadImage("Assets/Amylose3.png")
+    16 = loadImage("Assets/Amylose4.jpg")
+    17 = loadImage("Assets/Amylose5.png")
+    18 = loadImage("Assets/Amylose6.png")
+    19 = loadImage("Assets/Amylose7.jpg")
+    20 = loadImage("Assets/Amylose8.png")
+    21 = loadImage("Assets/Amylose9.png")
+    22 = loadImage("Assets/Amylose10.png")
+    23 = loadImage("Assets/Amylose11.png")
+    24 = loadImage("Assets/Amylose12.png")
 }
     // creating every interactable
 
@@ -36,6 +37,7 @@ function setup(){
     startButton = rect(200, 125, 400, 1350)
     amyloseButton = rect(200, 200, 400, 1200)
     amylopectinButton = rect(200, 200, 800, 1200)
+    amyloPicture = createSprite(400, 400, 400, 400)
 }
 
     // setting up the game
@@ -43,20 +45,64 @@ function setup(){
 function draw(){
     background(255);
 
-    rightAnswerArray = [1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2]
-    questionAskRandomizer = randomNumber(0,23)
+
+
     
 
 
 
 
     if(gameState = play){
-
+        startButton.visible = false;
+        amyloseButton.visible = true;
+        amylopectinButton.visible = true;
+        randomNumberPicker();
+        if(mousePressedOver(amyloseButton)){
+            if(rightAnswerArray[rand] = 2){
+                score++;
+                randomNumberPicker();
+            } else if(rightAnswerArray[rand] = 1){
+                gameState = loss;
+                randomNumberPicker();
+            }
+        }if(mousePressedOver(amylopectinButton)){
+            if(rightAnswerArray[rand] = 1){
+                score++;
+                randomNumberPicker();
+            } else if(rightAnswerArray[rand] = 2){
+                gameState = loss;
+                randomNumberPicker();
+            }
+        }
     } 
     else if(gameState = idle){
+        startButton.visible = true;
+        amyloseButton.visible = false;
+        amylopectinButton.visible = false;
         
+        if(mousePressedOver(startButton)){
+            reset();
+        }
     }
     else if(gameState = loss){
-
+        startButton.visible = true;
+        amyloseButton.visible = false;
+        amylopectinButton.visible = false;
+        if(mousePressedOver(startButton)){
+            reset();
+        }
     }
+}
+
+function reset(){
+    gameState = play;
+    score = 0;
+}
+
+function randomNumberPicker(){    
+    rightAnswerArray = [1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2]
+    rand = Math.round(random(0,23));
+    rightAnswerArray[rand]
+    amyloPicture.addImage(rand)
+
 }
